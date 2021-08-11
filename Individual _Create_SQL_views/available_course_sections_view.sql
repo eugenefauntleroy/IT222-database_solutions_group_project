2fc5fb2bd96f1f85dfb11f95d96c79def31c113e
Esteban <26440947+esteban-gs@users.noreply.github.com>
2021-08-10 22:44:15 -0700

    Update available_course_sections_view.sql

--- Individual _Create_SQL_views/available_course_sections_view.sql
+++ Individual _Create_SQL_views/available_course_sections_view.sql
@@ -1,11 +1,11 @@
 -- 1: Available_course_sections_view
 -- DB: techcertificate
+
 CREATE OR REPLACE VIEW availablecoursesectionsview
 AS
 SELECT sectionkey
-	, sectionyear
+	, concat(sectionyear, '-', quarterkey, '-', quartername) as "Quarter Year"
 -- 	, quarterkey
-	, quartername
 -- 	, coursekey
 	, coursename
 	, coursedescription
@@ -22,5 +22,5 @@ SELECT sectionkey
 	JOIN person using(personkey)
 	JOIN quarter using(quarterkey)
 	JOIN pricehistory using(pricehistorykey)
-	WHERE sectionyear >= EXTRACT(YEAR from CURRENT_DATE)
-	AND quarterkey >= EXTRACT(QUARTER FROM CURRENT_DATE);
+-- 	WHERE sectionyear >= EXTRACT(YEAR from CURRENT_DATE)
+-- 	AND quarterkey >= EXTRACT(QUARTER FROM CURRENT_DATE);
