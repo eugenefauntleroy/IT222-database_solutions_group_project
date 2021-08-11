ea4a5afdef831cf821d731424561ce744dd0bb06
Esteban <26440947+esteban-gs@users.noreply.github.com>
2021-08-10 23:51:39 -0700

    Update studentsquartertuitionview.sql

--- Individual _Create_SQL_views/studentsquartertuitionview.sql
+++ Individual _Create_SQL_views/studentsquartertuitionview.sql
@@ -8,10 +8,11 @@ create or replace view studentsquartertuitionview
 as
 
 SELECT statusname as "Status"
+	, studentkey as "Student Id"
+	, concat(firstname, ' ', lastname) as "Student"
 	, studentstartdate as "Enrolled"
 	, coursename as "Course"
-	, quartername as "Quarter"
-	, sectionyear as "Year"
+	, concat(sectionyear, '-', quarterkey, '-', quartername) as "Quarter Year"
 	, credits as "Available Credits"
 	, pricepercredit as "Price/Credit"
 	, credits * pricepercredit as "Cost"
@@ -23,6 +24,6 @@ SELECT statusname as "Status"
 	join public.person using(personkey)
 	join public.status using(statuskey)
 	join public.pricehistory using(pricehistorykey)
-	where studentkey = 179
-	AND quarterkey >= EXTRACT(QUARTER FROM CURRENT_DATE)
+-- 	where studentkey = 179
+-- 	AND quarterkey >= EXTRACT(QUARTER FROM CURRENT_DATE)
 	order by sectionyear, quarterkey asc;
